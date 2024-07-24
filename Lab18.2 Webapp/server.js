3e774afa977ae03b3626cca1e6ac550f44c6c63e
Malay911 <160609243+Malay911@users.noreply.github.com>
2024-07-24 21:21:14 +0530

    Add files via upload

---
+++ Lab18.2 Webapp/server.js
@@ -0,0 +1,86 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+    if (filePath == './') {
+        filePath = './home.html';
+    }
+
+    const extname = String(path.extname(filePath)).toLowerCase();
+    const mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        // '.gif': 'image/gif',
+        // '.wav': 'audio/wav',
+        // '.mp4': 'video/mp4',
+        // '.woff': 'application/font-woff',
+        // '.ttf': 'application/font-ttf',
+        // '.eot': 'application/vnd.ms-fontobject',
+        // '.otf': 'application/font-otf',
+        // '.svg': 'application/image/svg+xml'
+    };
+
+    const contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            if (error.code == 'ENOENT') {
+                fs.readFile('./404.html', (error, content) => {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf-8');
+                });
+            } else {
+                res.writeHead(500);
+                res.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+                res.end();
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
+
+// const http = require('http');
+// const url = require('url');
+
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//     const parsedUrl = url.parse(req.url, true);
+//     const path = parsedUrl.pathname;
+
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+
+//     if (path === './') {
+//         res.write('<h1>Welcome to the Home Page!</h1>');
+//     } else if (path === './services.html') {
+//         res.write('<h1>This is the About Page!</h1>');
+//     } else if (path === './contact.html') {
+//         res.write('<h1>These are our Services!</h1>');
+//     } else if (path === './about.html') {
+//         res.write('<h1>Contact us here!</h1>');
+//     } else if (path === './FAQ.html') {
+//         res.write('<h1>This is another About Page!</h1>');
+//     } else {
+//         res.writeHead(404, { 'Content-Type': 'text/html' });
+//         res.write('<h1>404 Not Found</h1>');
+//     }
+
+//     res.end();
+// });
+
+// server.listen(port, () => {
+//     console.log(`Server is running on http://localhost:${port}`);
+// });
