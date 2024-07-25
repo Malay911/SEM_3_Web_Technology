b0565e579d8a89a9c0aa46ae02fba2d995562645
Malay911 <160609243+Malay911@users.noreply.github.com>
2024-07-25 08:41:28 +0530

    Update server.js

--- Lab18.2 Webapp/server.js
+++ Lab18.2 Webapp/server.js
@@ -84,3 +84,41 @@ server.listen(port, () => {
 // server.listen(port, () => {
 //     console.log(`Server is running on http://localhost:${port}`);
 // });
+
+const http = require('http');
+const server = http.createServer((req,res) => {
+    res.setHeader('Content-Type','text/html');
+
+    console.log(req.url);
+    
+    if(req.url == '/favicon.ico'){
+        res.end('');
+    }
+    else if(req.url == '/home'){
+        res.StatusCode = 200;
+        res.write('Welcome Home Buddy..');
+        res.write('</br>');
+        res.write('bye byeeee !!!!!!!!!!!');
+        res.end();
+    }
+    else if(req.url == '/Contact'){
+        res.StatusCode = 200;
+        res.end('My phone number is : <h2> 93280 #####</h2>');
+    }
+    else if(req.url == '/About'){
+        res.StatusCode = 200;
+        res.write('ABOUT US');
+        res.write("</br>");
+        res.end('This is all it is !!');
+    }
+    else{
+        res.write('Wrong url...');
+        res.write('</br>');
+        res.write('Try Again');
+        res.end();
+    }
+});
+
+    server.listen(4040,() =>{
+        console.log(`Server is running on http://localhost:${port}`);
+    });
