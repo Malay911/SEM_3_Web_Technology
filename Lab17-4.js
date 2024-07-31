a402d6b565cc6e969802726e21c435abeb6991a1
Malay911 <160609243+Malay911@users.noreply.github.com>
2024-07-31 18:09:51 +0530

    Update Lab17-4.js

--- Lab17-4.js
+++ Lab17-4.js
@@ -1,2 +1,9 @@
-const child_process = require('child_process')
-console.log(child_process)
+const child_process = require('child_process');
+
+// child_process.exec('dir', (err,stdout,stdin)=>{
+//     console.log(stdout);
+// });
+
+child_process.exec('JS.txt', (err,stdout,stdin)=>{
+    console.log(stdout);
+});
