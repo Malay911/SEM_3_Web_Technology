90380d459522f26a06501bdb55a775e4ecf08fa0
Malay911 <160609243+Malay911@users.noreply.github.com>
2024-08-22 09:20:43 +0530

    Delete CRUD_API directory

--- CRUD_API/student_two.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const schema = mongoose.Schema({
-    id: Number,
-    name: String,
-    age: Number
-});
-
-module.exports = mongoose.model("Demo",schema,"Students");
