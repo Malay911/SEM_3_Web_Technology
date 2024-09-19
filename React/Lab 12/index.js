c142028ae9fea360b50f19ddfe401c4ed441344e
Malay911 <160609243+Malay911@users.noreply.github.com>
2024-09-19 09:23:26 +0530

    Add files via upload

---
+++ React/Lab 12/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from './Lab12_Components.js/Home';
+import About from './Lab12_Components.js/About';
+import Services from './Lab12_Components.js/Services';
+import Contact from './Lab12_Components.js/Contact';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './Lab12_Components.js/Layout';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <>
+//     <App />
+//     <Home/>
+//     <Game/>
+//     <xyz/>
+//     <button onClick={()=>{
+//       root.render(<App/>)
+//     }} >Home</button>
+//   </>
+// );
+root.render(
+  <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />} >
+        <Route path="/Home" element={<Home/>}/>
+        <Route path="/About" element={<About/>}/>
+        <Route path="/Services" element={<Services/>}/>
+        <Route path="/Contact" element={<Contact/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </>
+);
