c247300c29e0e34bd5e3737705da3feb8394617c
Malay911 <160609243+Malay911@users.noreply.github.com>
2024-09-20 17:07:47 +0530

    Update index.js

--- Lab 15/index.js
+++ Lab 15/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Displaystudents from './Components/students';
+import Displayproducts from './Components/products';
+import Displayfaculties from './Components/faculties';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Displaystudents/>);
+root.render(
+    <>
+        <Displaystudents/><br/>
+        <Displayproducts/><br/>
+        <Displayfaculties/>
+    </>
+);
