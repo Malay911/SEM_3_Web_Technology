c91e720fe6b6b488b584ad0a5df41685b4894323
Malay Panara <160609243+Malay911@users.noreply.github.com>
2024-10-17 11:20:42 +0530

    Update Lab19-2.js

--- Lab19-2.js
+++ Lab19-2.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app=express();
 const isValid=false;
+
 app.get('/home',(req,res)=>{
     res.send('Welcome Home');
 });
+
 app.get('/contact',(req,res)=>{
     res.send('Welcome TO Contact');
 });
+
 app.get('/about',(req,res)=>{
     res.send('Welcome about');
 });
+
 app.get('/FAQ',(req,res)=>{
     res.send('FAQ');
 });
