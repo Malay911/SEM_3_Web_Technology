6db3c6d7ed41a79d5e6e757c8fdf610378ae8fc2
Malay Panara <160609243+Malay911@users.noreply.github.com>
2024-10-19 10:48:16 +0530

    Update Lab17-2.js

--- Lab17-2.js
+++ Lab17-2.js
@@ -4,6 +4,7 @@ const filePath = "D:\\Darshan University\\Data\\BTech\\Sem-3\\WT\\2024\\Codes\\A
 ans1 = path.dirname(filePath);
 ans2 = path.basename(filePath);
 ans3 = path.extname(filePath);
+
 console.log("dirname",ans1);
 console.log("basename",ans2);
 console.log("extname",ans3);
