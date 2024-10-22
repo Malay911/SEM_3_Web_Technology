4a537c14abe18e8a30eb7dcab49536205fe0e6fa
Malay Panara <160609243+Malay911@users.noreply.github.com>
2024-10-22 10:09:05 +0530

    Add files via upload

---
+++ Practice/url.js
@@ -0,0 +1,7 @@
+const url=require('url');
+const adr='https://www.darshan.ac.in/abcd.js?FirstName=Malay&LastName=Panara';
+const q=url.parse(adr,true);
+
+console.log(q.host);
+console.log(q.pathname);
+console.log(q.search);
