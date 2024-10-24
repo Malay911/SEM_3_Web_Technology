5c0c675b1109cfa418dd1153c2618a4fa76c2fcf
Malay Panara <160609243+Malay911@users.noreply.github.com>
2024-10-24 12:14:50 +0530

    Add files via upload

---
+++ Practice/path.js
@@ -0,0 +1,9 @@
+const path=require('path');
+
+console.log(path.normalize('/foo/abc/xyz'));
+console.log(path.join('/foo/bar','abc'));
+console.log(path.relative('/foo/bar','/foo/abc'));
+console.log(path.resolve('abc','xyz'));
+console.log(path.dirname('/foo/abc.txt'));
+console.log(path.basename('/foo/abc.txt'));
+console.log(path.extname('/foo/abc.txt'));
