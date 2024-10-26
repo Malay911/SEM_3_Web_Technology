3dfb2eb04a9123037a16f63ffe98f214ddec6a61
Malay Panara <160609243+Malay911@users.noreply.github.com>
2024-10-26 13:55:35 +0530

    Update Middleware.js

--- Practice/Middleware.js
+++ Practice/Middleware.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded());
 app.use('/admin',(req,res,next)=>{
     console.log("req.originalUrl",req.originalUrl);
     console.log("req.baseUrl",req.baseUrl);
+
     console.log("req.path",req.path);
     
     if(req.body.UN=="arjun" && req.body.PW=="bala"){
