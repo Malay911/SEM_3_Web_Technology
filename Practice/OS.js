c67e7b666a8e628bde269217c7d433b8f53cb5d7
Malay Panara <160609243+Malay911@users.noreply.github.com>
2024-10-31 14:55:06 +0530

    Update OS.js

--- Practice/OS.js
+++ Practice/OS.js
@@ -17,6 +17,7 @@ console.log(os.hostname());
 console.log(os.loadavg());
 
 console.log(os.networkInterfaces());
+
 console.log(os.platform());
 console.log(os.release());
 console.log(os.tmpdir());
